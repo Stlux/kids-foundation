6e3d27b6ec09dc419b6baf83e254fa9b3fa2710b
Kyrylo <kirilllikholetov3@gmail.com>
2025-04-11 13:30:26 +0200

    u

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,30 +3,39 @@ import s1 from "../assets/s1.png";
 import s2 from "../assets/s1.avif";
 import s3 from "../assets/s2.avif";
 import s4 from "../assets/s3.avif";
+import dan from "../assets/dan.jpg";
 
 function Footer() {
     return ( 
         <>
             <footer>
-                <div className="footer-up">
-                    <div className="container">
-                        <center>
-                            <div className="logo">
-                                <img src={logo} alt="" />
-                            </div>
-                            <h2>
-                                We will do everything to help children survive and thrive.
-                            </h2>
-                            <p>
-                                The WPA Foundation, a 501(c)(3) nonprofit organization that helps children fight cancer and offers financial and emotional support programs.
-                            </p>
-                            <div className="star-row">
-                                <img src={s1} alt="" />
-                                <img src={s2} alt="" />
-                                <img src={s3} alt="" />
-                                <img src={s4} alt="" />
-                            </div>
-                        </center>
+                <div className="row-footer">
+                    <div className="footer-up">
+                        <div className="container">
+                            <center>
+                                <div className="logo">
+                                    <img src={logo} alt="" />
+                                </div>
+                                <h2>
+                                    We will do everything to help children survive and thrive.
+                                </h2>
+                                <p>
+                                    The WPA Foundation, a 501(c)(3) nonprofit organization that helps children fight cancer and offers financial and emotional support programs.
+                                </p>
+                                <div className="star-row">
+                                    <img src={s1} alt="" />
+                                    <img src={s2} alt="" />
+                                    <img src={s3} alt="" />
+                                    <img src={s4} alt="" />
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div className="image-special-p">
+                        <img src={dan} alt="" />
+                        <p>
+                            Founder and CEO<br />Daniel Osterreich
+                        </p>
                     </div>
                 </div>
                 <hr />
@@ -40,6 +49,14 @@ function Footer() {
                         </div>
                     </div>
                 </div>
+                {/*}
+                <div className="image-special">
+                    <img src={dan} alt="" />
+                    <p>
+                        Founder and CEO<br />Daniel Osterreich
+                    </p>
+                </div>
+                {*/}
             </footer>
         </>
      );
